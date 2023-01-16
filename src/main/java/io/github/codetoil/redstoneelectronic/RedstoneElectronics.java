51b88c6c2b3f160f52f08bfab31efa3f7a124a9f
Anthony (Codetoil) <ianthisawesomee@gmail.com>
2023-01-16 20:00:41 +0000

    Add license to the rest of the mod

--- src/main/java/io/github/codetoil/redstoneelectronic/RedstoneElectronics.java
+++ src/main/java/io/github/codetoil/redstoneelectronic/RedstoneElectronics.java
@@ -1,3 +1,21 @@
+/**
+ *  Redstone Electronics is a MC Mod that adds restone components.
+ *  Redstone Electronics (C) 2020-2023  Codetoil
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.codetoil.redstoneelectronic;
 
 import io.github.codetoil.redstoneelectronic.blocks.REBlocks;
