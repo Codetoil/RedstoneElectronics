7e9eaa57f1f95e31f654e18be2554a456f8d2cf8
Anthony (Codetoil) <ianthisawesomee@gmail.com>
2023-01-18 14:02:57 -0500

    disp fold

--- src/main/java/io/github/codetoil/redstoneelectronics/RedstoneElectronics.java
+++ src/main/java/io/github/codetoil/redstoneelectronics/RedstoneElectronics.java
@@ -59,6 +59,7 @@ public class RedstoneElectronics {
 
     private void gatherData(GatherDataEvent event) {
         DataGenerator gen = event.getGenerator();
+        logger.info(gen.getOutputFolder());
         if (event.includeReports())
         {
             gen.addProvider(new BlockListReport(gen));
